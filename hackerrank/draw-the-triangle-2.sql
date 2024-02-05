ec74d7693f068927e8326a72f157b264f30b9de9
Aleh Mikhalchuk <OlegMikhalchuk@coherentsolutions.com>
2024-02-05 16:38:19 +0100

    draw-the-triangle-2

---
+++ hackerrank/draw-the-triangle-2.sql
@@ -0,0 +1,36 @@
+-- https://www.hackerrank.com/challenges/draw-the-triangle-2/problem
+/*
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+*
+* * 
+* * * 
+* * * *
+* * * * *
+Write a query to print the pattern P(20).
+*/
+
+with nums as (
+    select 1 num union all
+    select 2 num union all
+    select 3 num union all
+    select 4 num union all
+    select 5 num union all
+    select 6 num union all
+    select 7 num union all
+    select 8 num union all
+    select 9 num union all
+    select 10 num union all
+    select 11 num union all
+    select 12 num union all
+    select 13 num union all
+    select 14 num union all
+    select 15 num union all
+    select 16 num union all
+    select 17 num union all
+    select 18 num union all
+    select 19 num union all
+    select 20 num
+)
+
+select REPLICATE('* ', num)
+from nums;
